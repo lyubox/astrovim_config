e99c8e6358f08789ab634a20c4c4398d98c7376f
Lyubo Nay <lyubox@LT406453.asml.com>
2024-02-27 01:07:35 +0200

    Initial config

--- init.lua
+++ init.lua
@@ -33,7 +33,8 @@ return {
       format_on_save = {
         enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
-          -- "go",
+          "javascript",
+          "json"
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,16 @@
+-- TODO: test
 return {
+  {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  event = "User AstroFile",
+  cmd = {"TodoQuickFix"}
+}
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
